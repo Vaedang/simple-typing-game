8729b45850027737d428d45cc31951d4af04a280
Vaedang <44874068+Vaedang@users.noreply.github.com>
2019-02-11 22:56:29 +0700

    update code

---
+++ Assets/ScriptWord/WordGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+   private static string[] wordList = { "testy","treatment","crack","immense","ray","sidewalk"
+            ,"flash","jolly","sulky","redundant","shade","snobbish","spooky","anger","park","accessible"
+            ,"gorgeous","seemly","milk","inject","carriage","early","whip","side","egg"
+            ,"telephone","imported","unpack","crowd","gaze","burst","boat","envious","aftermath","worried","tumble","tempt"
+            ,"nod","hard","amount","retire","end","canvas","wail","aunt","aromatic","slimy","large","determined","nappy" };
+   public static string GetRandomWord()
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+        return randomWord;
+    }
+}
---
+++ Assets/ScriptWord/WordSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordSpawner : MonoBehaviour
+{
+    public GameObject wordPrefab;
+    public Transform wordCanvas;
+
+   public WordDisplay SpawWord()
+    {
+        Vector3 randomPosition = new Vector3(-8f,Random.Range(-3f,3f));
+
+
+        GameObject wordObj = Instantiate(wordPrefab,randomPosition,Quaternion.identity, wordCanvas);
+        WordDisplay wordDisplay = wordObj.GetComponent<WordDisplay>();
+
+        return wordDisplay;
+    }
+}
